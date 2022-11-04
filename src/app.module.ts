b332c8f0bbc28ab5f92cef166a6cc69b2bd645ad
Rokas <dailyuseracc1@gmail.com>
2022-11-04 10:47:55 +0200

    dog-schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { DogModule } from './dog/dog.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    PrismaModule,
+    DogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
